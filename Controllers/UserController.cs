da3280e0b8f8af6d9c2daacffd5936ddbeb4f239
Admin <Admin@SRKI-153>
2026-09-07 10:59:17 +0530

    USER_UPDATE

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Movie_Mngt_System.Models;
 
 namespace Movie_Mngt_System.Controllers
 {
     public class UserController : Controller
     {
+
+        public int Get_User_id()
+        {
+            int id = 0;
+            if (Session["Email"] != null)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand("Get_User", connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                connection.Open();
+                cmd.Parameters.AddWithValue("@Email_id", Session["Email"]);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    id = Convert.ToInt32(reader["User_id"]);
+                }
+
+            }
+            return id;
+        }
         // GET: User
         public ActionResult Index()
         {
@@ -43,24 +68,62 @@ namespace Movie_Mngt_System.Controllers
         }
 
         // GET: User/Edit/5
-        public ActionResult Edit(int id)
+        public ActionResult Edit()
         {
-            return View();
+            User use = new User();
+            if (Session["Email"] != null)
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+                SqlConnection connection = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand("Get_User", connection);
+                cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                connection.Open();
+                cmd.Parameters.AddWithValue("@Email_id", Session["Email"]);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+                    use.user_id = Convert.ToInt32(reader["User_id"]);
+                    use.user_name = reader["User_name"].ToString();
+                    use.email_id = reader["Email_id"].ToString();
+                    use.user_password = reader["User_password"].ToString();
+                    use.city = reader["City"].ToString();
+                    use.phone_number = reader["PhoneNo"].ToString();
+                }
+                connection.Close();
+            }
+            return View(use);
         }
 
         // POST: User/Edit/5
         [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
+        public ActionResult Edit(User use)
         {
             try
             {
                 // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    string connectionString = ConfigurationManager.ConnectionStrings["dbconnection"].ToString();
+                    SqlConnection connection = new SqlConnection(connectionString);
+                    SqlCommand cmd = new SqlCommand("Update_User", connection);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.Parameters.AddWithValue("@User_id", Get_User_id());
+                    cmd.Parameters.AddWithValue("@User_name", use.user_name);
+                    cmd.Parameters.AddWithValue("@Email_id", use.email_id);
+                    cmd.Parameters.AddWithValue("@User_password", use.user_password);
+                    cmd.Parameters.AddWithValue("@City", use.city);
+                    cmd.Parameters.AddWithValue("@PhoneNo", use.phone_number);
+                    cmd.ExecuteNonQuery();
+                    ViewBag.Message = "Update Successfully!";
+                    connection.Close();
+                }
+                return View(use);
             }
-            catch
+            catch (Exception ex)
             {
-                return View();
+                ViewBag.Error = ex + " Error to Update profile";
+                return View(use);
             }
         }
 
